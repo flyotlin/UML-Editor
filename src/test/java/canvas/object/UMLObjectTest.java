a66424b0d5f1d873e2dbc887394809884d87fbd4
flyotlin <flyotlin@gmail.com>
2022-05-28 20:30:07 +0800

    [Test] - Add junit and test template for UMLObject

---
+++ src/test/java/canvas/object/UMLObjectTest.java
@@ -0,0 +1,31 @@
+package canvas.object;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UMLObjectTest {
+
+    @org.junit.jupiter.api.Test
+    void moveObject() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createAnchorPoints() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeAnchorPoints() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void decideLineConnectPoint() {
+//        UMLObject obj = new UMLObject();
+    }
+
+    @org.junit.jupiter.api.Test
+    void addHeadLines() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addTailLines() {
+    }
+}
