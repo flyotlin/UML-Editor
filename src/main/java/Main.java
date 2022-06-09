222cfd902936e6cb4ee043a590ec29d02aded32f
flyotlin <flyotlin@gmail.com>
2022-06-09 14:09:59 +0800

    [Refactor] - Remove the old UML editor code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,7 @@
-import editor.Editor;
 import future.UMLEditor;
 
 public class Main {
-    private static int frameWidth = 1200;
-    private static int frameHeight = 800;
     public static void main(String[] args) {
-//        Editor.run(frameWidth, frameHeight);
         UMLEditor.getInstance().run();
     }
 }
